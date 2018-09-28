5fd8e97f27a7bcfe8fd8a75d90a6f78c9a333d02
Joe Meyer <jmeyer@smm.org>
2018-09-28 09:37:22 -0500

    pins corrected for my board

--- local/src/app.js
+++ local/src/app.js
@@ -1,5 +1,5 @@
 const Gpio = require('onoff').Gpio;
-const led = new Gpio(17, 'out');
-const button = new Gpio(4, 'in', 'both');
+const led = new Gpio(11, 'out');
+const button = new Gpio(13, 'in', 'both');
 
 button.watch((err, value) => led.writeSync(value));
