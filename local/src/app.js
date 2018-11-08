003383e891e1ff81ab0ea21d779dce1b1aab9022
Joe Meyer <jmeyer@smm.org>
2018-11-08 13:42:21 -0600

    beam in array

--- local/src/app.js
+++ local/src/app.js
@@ -1,12 +1,10 @@
-var onoff = require('onoff'); //#A
+var onoff = require('onoff'); // Import the onoff library
 
 var Gpio = onoff.Gpio,
 
-  solenoid = new Gpio(15, 'out'); //output triggering relay for solenoid releasing cars.
-  beam0 = new Gpio(14, 'in', 'falling'); //Initialize beam breaks
-  beam1 = new Gpio(2, 'in', 'falling');
-  beam2 = new Gpio(3, 'in', 'falling');
-  beam3 = new Gpio(4, 'in', 'falling');
+solenoid = new Gpio(15, 'out'); //output triggering relay for solenoid releasing cars.
+beam = [new Gpio(2, 'in', 'falling'), new Gpio(3, 'in', 'falling'), new Gpio(4, 'in', 'falling')]
+beam0 = new Gpio(14, 'in', 'falling'); //Initialize beam breaks
 
 var replicate = 1;
 var raceName = "blank";
@@ -16,10 +14,8 @@ function startRace(){
   raceTimes = [];
   var timeNow = (new Date()).getTime();
   raceTimes[0] = timeNow;
-  //console.log("Solenoid release Time: " + timeNow);
-  var timeNow = (new Date()).getTime();
   solenoid.writeSync(1); //set pin state to 1(power solenoid)
-  setTimeout(offSolenoid, 1000); //release solenoid after 3 seconds
+  setTimeout(offSolenoid, 1000); //release solenoid after 1 second
   var currentRaceName = document.getElementById("tbRaceName").value;
   if (currentRaceName != raceName){
     replicate = 1;
@@ -43,7 +39,8 @@ beam0.watch((err, value) => {
   }
 });
 
-beam1.watch((err, value) => {
+beam.watch((err, value) => {
+  console.log(value);
   if (err) {
     throw err;
   }
@@ -52,28 +49,6 @@ beam1.watch((err, value) => {
   } else {
     raceTimes[3] = (new Date()).getTime();
   }
-});
-beam2.watch((err, value) => {
-  if (err) {
-    throw err;
-  }
-  if (raceTimes[5]){
-    raceTimes[6] = (new Date()).getTime();
-  } else {
-    raceTimes[5] = (new Date()).getTime();
-  }
-});
-beam3.watch((err, value) => {
-  if (err) {
-    throw err;
-  }
-  if (raceTimes[7]>1){
-    raceTimes[8] = (new Date()).getTime();
-    endRace();
-  } else {
-    raceTimes[7] = (new Date()).getTime();
-  }
-
 });
 
 function endRace(){
@@ -91,18 +66,10 @@ function endRace(){
   raceTimes.length = 0;
 }
 
-process.on('SIGINT', function () { //#F
+process.on('SIGINT', function () { //Listen to the event triggered on CTRL+C
   clearInterval(interval);
-  Solenoid.writeSync(0); //#G
+  Solenoid.writeSync(0); //Cleanly close the GPIO pin before exiting
   solenoid.unexport();
   console.log('Bye, bye!');
   process.exit();
 });
-
-// #A Import the onoff library
-// #B Initialize pin 4 to be an output pin
-// #C This interval will be called every 2 seconds
-// #D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
-// #E Asynchronously write the new value to pin 4
-// #F Listen to the event triggered on CTRL+C
-// #G Cleanly close the GPIO pin before exiting
